4a2e0408d1e7eb018fb7af3dcb41dcbc37e3a336
burkayelbek <burkay.elbek@gmail.com>
2022-07-19 21:46:45 +0300

    Props with components

--- src/App.js
+++ src/App.js
@@ -1,23 +1,33 @@
 import logo from './logo.svg';
 import './App.css';
+import Expenses from './components/Expenses'
 
 function App() {
+  const expenses = [
+    {
+      id:'e1',
+      title:'Car Insurance',
+      amount:294.67,
+      date:new Date(2021,2 ,28),
+    },
+    {
+      id:'e2',
+      title:'Car Test',
+      amount:294.67,
+      date: new Date(2021,3 ,28),
+    },
+    {
+      id:'e3',
+      title:'Car Desk',
+      amount:294.67,
+      date:new Date(2021,5 ,28),
+    }
+  ];
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h2>Lets get started!</h2>
+      <Expenses items={expenses}/>
     </div>
   );
 }
