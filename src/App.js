c7f4296e65b83b6e42b1793211ec929a195f22fd
burkayelbek <burkay.elbek@gmail.com>
2022-08-20 18:03:06 +0300

    NewExpense form added seperately

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import logo from './logo.svg';
 import './App.css';
 import Expenses from './components/Expenses/Expenses'
+import NewExpense from './components/NewExpense/NewExpense';
 
 function App() {
   const expenses = [
@@ -26,7 +27,7 @@ function App() {
 
   return (
     <div>
-      <h2>Lets get started!</h2>
+      <NewExpense/>
       <Expenses items={expenses}/>
     </div>
   );
